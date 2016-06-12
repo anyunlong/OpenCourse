fc49c9136e6ff906727721115f291b29ccb46899
Oneself <825920315@qq.com>
2016-06-12 14:38:57 +0800

    修改前缀

---
+++ OpenCourse/Classes/Search/Controller/OCESearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  OCESearchViewController.h
+//  OpenCourse
+//
+//  Created by Oneself on 16/6/9.
+//  Copyright © 2016年 CCUT. All rights reserved.
+//
+
+#import "AYLTableViewController.h"
+
+@interface OCESearchViewController : AYLTableViewController
+
+@end
---
+++ OpenCourse/Classes/Search/View/OCESearchView.m
@@ -0,0 +1,67 @@
+//
+//  OCESearchView.m
+//  OpenCourse
+//
+//  Created by Oneself on 16/5/31.
+//  Copyright © 2016年 CCUT. All rights reserved.
+//
+
+#import "OCESearchView.h"
+// category
+#import "UIImage+AYLExtension.h"
+#import "UIView+AYLExtension.h"
+
+static const CGFloat kTitleLableFontSize = 13;
+extern const CGFloat kUIViewAYLExtensionViewMargin;
+
+@interface OCESearchView()
+
+@end
+
+@implementation OCESearchView
+
++ (instancetype)searchView {
+    return [[self alloc] init];
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        // 圆角边缘
+        [self ayl_circularBeadViewWithRadius:5];
+        
+        // 背景图片
+        self.background = [UIImage ayl_resizedImageWithName:@"search_list_bg"];
+        
+        // 左边的放大镜图标
+        UIImage *iconImg = [UIImage imageNamed:@"search_fdj"];
+        UIImageView *iconView = [[UIImageView alloc] initWithImage:iconImg];
+        iconView.contentMode = UIViewContentModeCenter;
+        self.leftView = iconView;
+        self.leftViewMode = UITextFieldViewModeAlways;
+        
+        // 右边的清除按钮
+        self.clearButtonMode = UITextFieldViewModeAlways;
+        
+        // 字体
+        self.font = [UIFont systemFontOfSize:kTitleLableFontSize];
+        
+        // 光标颜色
+        self.tintColor = [UIColor whiteColor];
+        
+        // 设置提醒文字
+        NSMutableDictionary *attr = [NSMutableDictionary dictionary];
+        attr[NSForegroundColorAttributeName] = [UIColor whiteColor];
+        self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"拖延症怎么治" attributes:attr];
+        
+        // 文字颜色
+        self.textColor = [UIColor whiteColor];
+        
+        // 设置左边图标的frame
+        CGSize iconImgSize = iconImg.size;
+        self.leftView.ayl_size = CGSizeMake(iconImgSize.width + 2 * kUIViewAYLExtensionViewMargin, self.ayl_height);
+    }
+    
+    return self;
+}
+
+@end
